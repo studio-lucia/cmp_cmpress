828e44e7ad83af20cad3bdfcf6927c24e1330565
Misty De Meo <mistydemeo@gmail.com>
2018-02-17 00:16:38 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::PathBuf;
+use std::process::exit;
+
+#[macro_use] extern crate quicli;
+extern crate sega_cmp;
+
+use quicli::prelude::*;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(short = "t", default_value = "8",
+                help = "Compression type (one of 8, 16, 32)")]
+    compression_size: u8,
+    #[structopt(short = "f", default_value = "0",
+                help = "Byte offset in input file to begin compression")]
+    offset: usize,
+    #[structopt(short = "s",
+                help = "Maximum number of bytes to compress")]
+    max_bytes: Option<usize>,
+    #[structopt(help = "Input file", parse(from_os_str))]
+    input: PathBuf,
+    #[structopt(help = "Output file", parse(from_os_str))]
+    output: PathBuf,
+}
+
+main!(|args: Opt| {
+    let size;
+    match args.compression_size {
+        8  => size = sega_cmp::Size::Byte,
+        16 => size = sega_cmp::Size::Word,
+        32 => size = sega_cmp::Size::Longword,
+        _  => {
+            println!("Compression size must be one of 8, 16, or 32! (Provided: {})",
+                     args.compression_size);
+            exit(1);
+        }
+    }
+
+    if !args.input.exists() {
+        println!("Input file {} does not exist!", args.input.to_str().unwrap());
+        exit(1);
+    }
+
+    let mut input_file;
+    match File::open(&args.input) {
+        Ok(f) => input_file = f,
+        Err(e) => {
+            println!("Error while trying to open file {}: {}", args.input.to_str().unwrap(), e);
+            exit(1);
+        }
+    }
+
+    let mut input_data = vec![];
+    input_file.read_to_end(&mut input_data)?;
+
+    let output_header = sega_cmp::create_header(input_data.len() as i32, size);
+    let output_data = sega_cmp::compress(&input_data, size)?;
+
+    let mut output_file;
+    match File::create(&args.output) {
+        Ok(f) => output_file = f,
+        Err(e) => {
+            println!("Error writing to file {}: {}", args.output.to_str().unwrap(), e);
+            exit(1);
+        }
+    }
+    output_file.write_all(&output_header)?;
+    output_file.write_all(&output_data)?;
+});
